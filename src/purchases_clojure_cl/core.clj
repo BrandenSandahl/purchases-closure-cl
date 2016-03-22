1f176ead57c36d7e33c8deaf6ee3b3128b1d53cc
BrandenSandahl <branden157@gmail.com>
2016-03-22 14:44:52 -0400

    now runs in a loopy loop

--- src/purchases_clojure_cl/core.clj
+++ src/purchases_clojure_cl/core.clj
@@ -6,30 +6,24 @@
 
 
 (defn -main []
-  
-  
-      (println "Enter a category to sort by.")
-
-  (let [purchases (slurp "purchases.csv")
-        purchases (str/split-lines purchases)
-        purchases (map (fn [line]
-                         (str/split line #","))
-                       purchases)
-        header (first purchases)
-        purchases (rest purchases)
-        purchases (map (fn [line]
-                         (apply hash-map (interleave header line)))
-                       purchases)
-        purchases (walk/keywordize-keys purchases)
-        text (read-line)
-        purchases (filter (fn [line]
-                           (= (:category line) text))
-                   purchases)]
-        
-
-   (spit (format "Purchases_By_%s.edn" text) (pr-str purchases))))
-            
-
+  (println "Enter a category to sort by.")
+  (loop []
+    (let [text (read-line)
+          purchases (slurp "purchases.csv")
+          purchases (str/split-lines purchases)
+          purchases (map (fn [line]
+                           (str/split line #","))
+                     purchases)
+          header (first purchases)
+          purchases (rest purchases)
+          purchases (map (fn [line]
+                           (apply hash-map (interleave header line)))
+                     purchases)
+          purchases (walk/keywordize-keys purchases)
+          purchases (filter (fn [line]
+                              (= (:category line) text))
+                     purchases)]
+      (when (not= text "q")
+        (spit (format "Purchases_By_%s.edn" text) (pr-str purchases))
+        (recur)))))
          
-        
-  
