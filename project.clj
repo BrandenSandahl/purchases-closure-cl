a2a7dbde7e9c4c6e06c433c17262cc7489e56e8c
BrandenSandahl <branden157@gmail.com>
2016-03-23 20:57:36 -0400

    Did all the things

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject purchases-clojure-cl "0.0.1-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :description "reading from a .csv and putputting to a webpage"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring "1.4.0"]
+                 [compojure "1.5.0"]
+                 [hiccup "1.0.5"]]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :aot [purchases-clojure-cl.core]
   :main purchases-clojure-cl.core)
--- src/purchases_clojure_cl/core.clj
+++ src/purchases_clojure_cl/core.clj
@@ -1,15 +1,14 @@
 (ns purchases-clojure-cl.core
-
-  (:require [clojure.string :as str]
-            [clojure.walk :as walk])
+  (:require [clojure.string :as str] ;this brings in a new lib
+            [clojure.walk :as walk] ;brings in walk lib 
+            [compojure.core :as c]
+            [ring.adapter.jetty :as j]
+            [ring.middleware.params :as p]
+            [hiccup.core :as h])
   (:gen-class))
 
-
-(defn -main []
-  (println "Enter a category to sort by.")
-  (loop []
-    (let [text (read-line)
-          purchases (slurp "purchases.csv")
+(defn read-purchases []
+    (let [purchases (slurp "purchases.csv")
           purchases (str/split-lines purchases)
           purchases (map (fn [line]
                            (str/split line #","))
@@ -19,11 +18,60 @@
           purchases (map (fn [line]
                            (apply hash-map (interleave header line)))
                      purchases)
-          purchases (walk/keywordize-keys purchases)
-          purchases (filter (fn [line]
-                              (= (:category line) text))
-                     purchases)]
-      (when (not= text "q")
-        (spit (format "Purchases_By_%s.edn" text) (pr-str purchases))
-        (recur)))))
-         
+          purchases (walk/keywordize-keys purchases)]
+      purchases))
+
+
+(defn categories [purchases]
+  (let [all-categories (map :category purchases)
+        categories-set (sort (set all-categories))]
+    [:div 
+     (map (fn [category]
+            [:span
+             [:a {:href (str "/?category=" category)} category]
+             " "])
+      categories-set)
+     [:span
+      [:a {:href "/?category=All"} "All"]]]))
+
+
+(defn header-html [purchases]
+  [:tr 
+   (let [header-values (keys (first purchases))]
+       (map (fn[item]
+              [:th (name item)])
+         header-values))])
+
+
+(defn purchases-html [filtered-purchases]
+   (map (fn [purchase]
+          [:tr
+           (map (fn [item]
+                  [:td (val item)])
+            purchase)]) 
+    filtered-purchases))
+
+(c/defroutes app    ;define routes here. The app part names the routes for the server to use
+  (c/GET "/" request
+    (let [params (:params request)
+          category (get params "category")
+          purchases (read-purchases)
+          filtered-purchases (filter (fn [purchase]
+                                       (= (:category purchase) category))
+                               purchases)]
+      (h/html 
+             [:head
+              [:link {:rel "stylesheet" :href "http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"}
+               [:html
+                [:body
+                 [:div {:class "container"}
+                  (categories purchases)
+                  [:table {:class "table-condensed"}               
+                   [:thead
+                    (header-html purchases)
+                     [:tbody                   
+                      (purchases-html (if (not= category "All") filtered-purchases purchases))]]]]]]]]))))
+                
+
+(defn -main []
+  (j/run-jetty (p/wrap-params app) {:port 3000}))   ;params for the web server
